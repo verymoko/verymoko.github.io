1a2031dc3274669000a9b5035a8f68ae1bea342d
verymoko <verymoko@gmail.com>
2021-06-02 23:47:50 +0300

    no.html, no.css. Semicolon → comma in meta

--- no.js
+++ no.js
@@ -3,13 +3,13 @@ let character = ' ';
 
 function spacePosition(character) {
   let initialText = document.getElementById('textarea').value;
-  
+
   let indexes = [];
   let index = initialText.indexOf(character, 0);
 
 console.clear();
 console.log(initialText);
-  
+
   while (index !== -1) {
     indexes.push(index);
     index = initialText.indexOf(character, index + 1);
@@ -18,6 +18,6 @@ console.log(initialText);
 
   let randomCharacterPosition = indexes[Math.floor(Math.random() * indexes.length)];
   let newText = initialText.slice(0, randomCharacterPosition) + ' ' + addedText + initialText.slice(randomCharacterPosition);
-  
+
   document.getElementById('textarea').value = newText;
 }
