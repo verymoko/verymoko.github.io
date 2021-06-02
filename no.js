f75742cc43583a661174df22c07624271e6042aa
verymoko <verymoko@gmail.com>
2021-06-03 00:30:41 +0300

    no.js: deleted console.log. no.html, css: added links

--- no.js
+++ no.js
@@ -7,14 +7,10 @@ function spacePosition(character) {
   let indexes = [];
   let index = initialText.indexOf(character, 0);
 
-console.clear();
-console.log(initialText);
-
   while (index !== -1) {
     indexes.push(index);
     index = initialText.indexOf(character, index + 1);
   }
-  console.log(indexes);
 
   let randomCharacterPosition = indexes[Math.floor(Math.random() * indexes.length)];
   let newText = initialText.slice(0, randomCharacterPosition) + ' ' + addedText + initialText.slice(randomCharacterPosition);
